849b4a2e9f36826b064d483a185a62856f0373ae
unknown <valeriapostica5@gmail.com>
2023-07-27 19:28:55 +0300

    Am initializat proiectul

---
+++ src/App.vue
@@ -0,0 +1,222 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <header class="navtab_container">
+    <a class="navtab_item1" href="index.html"
+      ><img src="Img/logo.png" alt="Logo" />
+    </a>
+    <nav class="navtab_item2">
+      <ul class="tabmenu_container">
+        <li><a href="index.html">Acasa</a></li>
+        <li><a href="index.html">Despre noi</a></li>
+        <li><a href="index.html">Galerie</a></li>
+        <li><a href="index.html">Contacte</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section class="bigS">
+    <img src="Img/jdjdj.jpg" alt="Main" />
+  </section>
+
+  <section class="info">
+    <h2 id="text">Enjoy Your New Experience</h2>
+    <a href="#" id="btn">Learn More</a>
+  </section>
+
+  <div class="sec">
+    <h2>Parallax Scroll</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. At possimus,
+      voluptatibus ipsum maiores dolorum ex facilis nesciunt harum accusantium
+      dolore iusto et nihil numquam fugit officia ducimus delectus blanditiis
+      doloremque. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Reiciendis natus placeat rerum totam perspiciatis porro laudantium eos,
+      ratione nihil voluptates voluptatem sint dolor iusto at maxime tempora
+      eaque. Perferendis, quae! Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Quasi reprehenderit illum voluptatibus consectetur
+      aperiam quas eligendi, incidunt laudantium, debitis quis sapiente nesciunt
+      odit modi dolorum illo autem eveniet iure temporibus. Lorem ipsum dolor
+      sit amet, consectetur adipisicing elit. Perspiciatis impedit iure at, nam
+      harum dolorum iusto aliquam dolor magni recusandae quia maxime laboriosam
+      unde et natus molestias mollitia provident libero. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Maxime quis tempore aliquid reiciendis
+      omnis ratione architecto consectetur quas voluptatibus quisquam veniam, ut
+      modi saepe sit nesciunt soluta dolores corrupti quod. Lorem ipsum dolor
+      sit, amet consectetur adipisicing elit. Molestiae soluta, quam ab labore
+      officia rem maiores amet, quis dolore consequuntur eaque! Quae modi
+      voluptatum ea, eos corporis consequuntur nihil nemo. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Voluptatum quaerat doloribus assumenda
+      autem dolor eaque, ut optio quo voluptatem distinctio totam reprehenderit
+      quibusdam facilis dolores deleniti error, eius ducimus temporibus. Lorem
+      ipsum dolor sit amet consectetur, adipisicing elit. Ea atque corporis
+      reprehenderit, cumque ducimus repellat ex officiis fugiat quam ad ut
+      maxime quisquam doloremque minima nesciunt necessitatibus omnis aspernatur
+      obcaecati!
+    </p>
+  </div>
+  <section class="gallery">
+    <img src="img/portfolio/1.jpg" alt="sksksk" />
+
+    <img src="img/portfolio/2.jpg" alt="sksksk" />
+
+    <img src="img/portfolio/3.jpg" alt="sksksk" />
+
+    <img src="img/portfolio/4.jpg" alt="sksksk" />
+
+    <img src="assets/portfolio/5.jpg" alt="sksksk" />
+
+    <img src="img/portfolio/6.jpg" alt="sksksk" />
+  </section>
+  <section class="bigS">
+    <img src="Img/studentw.png" alt="Main" />
+  </section>
+
+  <footer>
+    <section class="contain">
+      <div class="Contact">
+        <h3>Contact</h3>
+        <p>
+          Adresa:dhdhddg@gmail.com<br />
+          Phone:74747737
+        </p>
+      </div>
+    </section>
+    <section class="copyright">
+      Copyright &copy;Girls Go IT SummerCamp 2023 DumiHTML
+    </section>
+  </footer>
+
+</template>
+
+<style>
+/* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Noto+Sans&display=swap'); */
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgb(9, 9, 9);
+  border-radius: 50px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(254, 252, 252);
+}
+html {
+  background-color: black;
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: 0px;
+  font-family: Inter;
+}
+
+.navtab_container {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  background-color: rgb(7, 1, 36);
+}
+.navtab_container img {
+  height: 120px;
+  width: auto;
+}
+.tabmenu_container {
+  display: flex;
+  justify-content: space-between;
+  list-style-type: none;
+}
+.tabmenu_container li a {
+  text-decoration: none;
+  color: white;
+  padding: 30px;
+  text-transform: uppercase;
+  margin-right: 50px;
+  font-size: 20px;
+}
+.bigS img {
+  width: 100%;
+  object-fit: cover;
+  height: 700px;
+  object-position: top;
+  filter: brightness(20%);
+  position: relative;
+}
+.bigS::before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  background: linear-gradient(to top #0b0c53, transparent);
+  z-index: 1000;
+}
+.tabmenu_container li a:hover {
+  background: rgb(11, 27, 61);
+  color: rgb(208, 216, 238);
+}
+
+#text {
+  position: absolute;
+}
+
+#btn {
+  text-decoration: none;
+  display: inline-block;
+  padding: 8px 30px;
+  border-radius: 40px;
+  background: rgb(255, 250, 250);
+  color: rgb(0, 0, 0);
+  font-size: 1.5rem;
+}
+.sec {
+  position: relative;
+  padding: 100px;
+  background: rgb(7, 1, 36);
+}
+.sec h2 {
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+  color: white;
+}
+.sec p {
+  color: white;
+}
+.info h2 {
+  position: absolute;
+  text-align: center;
+  margin: 50px;
+  font-size: 3rem;
+  top: 45%;
+  z-index: 10;
+  color: white;
+}
+.info a {
+  position: absolute;
+  text-align: center;
+  margin: 50px;
+  font-size: 3rem;
+  top: 55%;
+  z-index: 10;
+  color: white;
+}
+.bigS {
+  width: 100%;
+  object-fit: cover;
+  height: 700px;
+  object-position: top;
+  filter: brightness(90%);
+  position: relative;
+}
+
+.gallery img {
+  max-width: 100px;
+  margin: auto;
+}
+</style>
---
+++ src/firebase/index.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSQ7207nWl1gjS9j43sM_xGRTTlaBliHg",
+  authDomain: "youthpath-14dd5.firebaseapp.com",
+  projectId: "youthpath-14dd5",
+  storageBucket: "youthpath-14dd5.appspot.com",
+  messagingSenderId: "221748414342",
+  appId: "1:221748414342:web:7f381557939ad26fcf4e07",
+  measurementId: "G-2HTMVE99ZX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+
+export {db}
