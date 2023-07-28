48284296619e56b7d90271c67407a6f6ab3a9da9
ValeriaPostica <140591432+ValeriaPostica@users.noreply.github.com>
2023-07-29 02:46:34 +0300

    poze

--- src/components/welcomeComp.vue
+++ src/components/welcomeComp.vue
@@ -1,8 +1,10 @@
 <template>
     <section class="bigS">
-    <img src="Img/jdjdj.jpg" alt="Main" />
+    <img src="./Img/jdjdj.jpg" alt="Main" />
   </section>
-
+  <section class="bigS">
+      <img src="./Img/studentw.png" alt="Main" />
+    </section>
   <section class="info">
     <h2 id="text">Enjoy Your New Experience</h2>
     <a href="#" id="btn">Learn More</a>
