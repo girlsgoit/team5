c8e9698f77e1b5f4e3965a1150d27c84a94fab09
ArinaVarticean <140591446+ArinaVarticean@users.noreply.github.com>
2023-07-29 14:12:05 +0300

    ari

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import header from "./components/header.vue"
 import footer from "./components/footer.vue"
 import informationComp from "./components/informationComp.vue"
 import welcomeComp from "./components/welcomeComp.vue"
+import cultura from "./components/cultura.vue"
 
 export const router = createRouter({
     history: createWebHistory(),
@@ -18,6 +19,12 @@ export const router = createRouter({
             path: "/welcomeComp",
             component: welcomeComp
         },
+    
+        {
+            name: "cultura",
+            path: "/cultura",
+            component: cultura
+        }
 
     ],
 });
